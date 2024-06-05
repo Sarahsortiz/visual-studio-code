deca77a5d5b917c1d0b90cc58f0e37d5f51d16b0
Sarahsortiz <sarahortiz2001@gmail.com>
2024-06-05 17:54:09 +0200

    Add files via upload

---
+++ s2l3 1es.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    int primo_numero;
+    int secondo_numero;
+    int moltiplicazione;
+
+    printf("motliplicazione tra i due numeri\n");
+
+    printf("inserire primo_numero\n");
+    scanf("%d, &primo_numero");
+    printf("inserire secondo_numero\n");
+    scanf("%d, &secondo_numero");
+
+    moltiplicazione= primo_numero * secondo_numero;
+
+    printf("il risultato della moltiplicazione è: %d\n prodotto");
+
+    return 0
+
+}
---
+++ s2l3 2s.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    int primo_numero;
+    int secondo_numero;
+    float media;
+
+    printf("calcolare media aritmetica\n")
+
+    printf("inserire primo_numero\n")
+    scanf("%d, &primo_numero")
+    printf("inserire second_numero\n")
+    scanf("%d, &secondo_numero")
+
+    media aritmetica = (primo_numero + secondo_numero) /2
+
+    printf("la media aritmetica è: %d\n, media aritmetica")
+    return 0
+}

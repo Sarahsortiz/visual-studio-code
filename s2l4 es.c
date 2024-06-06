7c8724924822e9866908a497c6366e5eaad03676
Sarahsortiz <sarahortiz2001@gmail.com>
2024-06-06 10:49:16 -0400

    Add files via upload

---
+++ s2l4 es.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+void print_menu;
+{
+    printf ("menu:\n")
+    printf ("A: Iniziare una nuova partita\n");
+    printf ("B: Uscire dal gioco\n");
+    printf ("scelta multipla")
+
+}
+
+int gioca_partita;{
+
+    int punteggio = 0;
+    char nome[26];
+    printf ("inserire nome utente\n");
+    scanf ("%d, &nome");
+
+
+    printf ("domanda1:\n");
+    printf ("inserire domanda\n");
+    printf ("A: risposta 1, B: risposta2, C: risposta3\n");
+    printf ("inserie risposta");
+    scanf ("%d, &risposta");
+    
+    
+    // punteggio in caso di risposta esatta
+    if (risposta esatta: "A || B || C"){
+        punteggio++;
+    }
+
+    printf ("domanda2:\n");
+    printf ("inserire domanda\n");
+    printf ("A: risposta 1, B: risposta2, C: risposta3\n");
+    printf ("inserie risposta");
+    scanf ("%d, &risposta");
+
+    // punteggio in caso di risposta esatta
+    if (risposta esatta: "A || B || C"){
+        punteggio++;
+    }
+
+    printf ("fine partita, punteggio totalizzato: %d, nome, punteggio\n")
+    return0
+
+
+
+}
